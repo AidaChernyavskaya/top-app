c6b4084835018a97a5499a03e304eb157a6939e2
Aida <aidachernyavskaya@gmail.com>
2022-12-16 14:03:54 +0300

    editable rating component is done

--- components/Rating/Rating.tsx
+++ components/Rating/Rating.tsx
@@ -1,7 +1,7 @@
 import styles from "./Rating.module.css";
 import cn from "classnames";
 import {RatingProps} from "./Rating.props";
-import {useEffect, useState} from "react";
+import {useEffect, useState, KeyboardEvent} from "react";
 import StarIcon from "./star.svg";
 
 export const Rating = ({isEditable = false, rating, setRating, ...props}: RatingProps): JSX.Element => {
@@ -14,14 +14,44 @@ export const Rating = ({isEditable = false, rating, setRating, ...props}: Rating
     const constructRating = (currentRating: number): void => {
         const updatedArray = ratingArray.map((r: JSX.Element, i: number) => {
             return (
-              <StarIcon
+              <span
                   className = {cn(styles.star, {
-                      [styles.filled]: i < currentRating
+                      [styles.filled]: i < currentRating,
+                      [styles.editable]: isEditable
                   })}
-              />
+                  onMouseEnter = {(): void => changeDisplay(i + 1)}
+                  onMouseLeave = {(): void => changeDisplay(rating)}
+                  onClick = {(): void => onClick(i + 1)}
+              >
+                  <StarIcon
+                      tabIndex = {isEditable ? 0 : -1}
+                      onKeyDown = {(e: KeyboardEvent<SVGElement>): void => {isEditable && handleSpace(i + 1, e)}}
+                  />
+              </span>
             );
         });
         setRatingArray(updatedArray);
+    };
+
+    const changeDisplay = (i: number): void => {
+        if (!isEditable){
+            return;
+        }
+        constructRating(i);
+    };
+
+    const onClick = (i: number): void => {
+        if (!isEditable || !setRating){
+            return;
+        }
+        setRating(i);
+    };
+
+    const handleSpace = (i: number, e: KeyboardEvent<SVGElement>): void => {
+        if (e.code != 'Space' || !setRating){
+            return;
+        }
+        setRating(i);
     };
 
     return (
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,8 @@ import {Button, Htag, Ptag, Rating, Tag} from '../components';
 
 
 export default function Home(): JSX.Element {
+    const [rating, setRating] = useState<number>(4);
+
     return (
     <>
         <Htag tag = 'h1'>Заголовок</Htag>
@@ -15,7 +17,7 @@ export default function Home(): JSX.Element {
         <Tag size={'m'} color={'red'}>Red</Tag>
         <Tag size={'s'} color={'green'}>Green</Tag>
         <Tag size={'s'} color={'primary'}>Primary</Tag>
-        <Rating rating={4}></Rating>
+        <Rating rating={rating} isEditable={true} setRating={setRating}/>
     </>
   );
 }
