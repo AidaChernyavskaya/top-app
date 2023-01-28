a8cd1113ddca32b6322bd9136ed3e8d46ace4c2f
Aida <aidachernyavskaya@gmail.com>
2023-01-28 21:15:27 +0300

    product component done

---
+++ components/Divider/Divider.tsx
@@ -0,0 +1,10 @@
+import styles from "./Divider.module.css";
+import cn from "classnames";
+import {DividerProps} from "./Divider.props";
+
+
+export const Divider = ({className, ...props}: DividerProps): JSX.Element => {
+    return (
+        <hr className={cn(className, styles.hr)} {...props}/>
+    );
+}
