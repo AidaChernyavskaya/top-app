f3321f18a3ce5726f06a413a9224b72da73907a8
Aida <aidachernyavskaya@gmail.com>
2023-01-28 14:58:46 +0300

    search component done

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -3,13 +3,14 @@ import cn from "classnames";
 import {SidebarProps} from "./Sidebar.props";
 import {Menu} from "../Menu/Menu";
 import Logo from "../logo.svg";
+import {Search} from "../../components";
 
 
 export const Sidebar = ({className, ...props}: SidebarProps): JSX.Element => {
     return (
         <div className={cn(className, styles.sidebar)} {...props}>
             <Logo className = {styles.logo}/>
-            <div>Поиск</div>
+            <Search />
             <Menu />
         </div>
     );
