bf1bc0352a3312eb63273c70761998b21b33cf87
Aida <aidachernyavskaya@gmail.com>
2023-01-30 21:35:13 +0300

    ButtonIcon component added

--- components/Up/Up.tsx
+++ components/Up/Up.tsx
@@ -1,9 +1,10 @@
 import styles from "./Up.module.css";
 import cn from "classnames";
-import UpIcon from "./up.svg";
+import UpIcon from "../ButtonIcon/up.svg";
 import {motion, useAnimation} from "framer-motion";
 import {useScrollY} from "../../hooks/useScrollY";
 import {useEffect} from "react";
+import {ButtonIcon} from "../ButtonIcon/ButtonIcon";
 
 
 export const Up = (): JSX.Element => {
@@ -22,13 +23,12 @@ export const Up = (): JSX.Element => {
     };
 
     return (
-        <motion.button
+        <motion.div
             className={styles.up}
-            onClick={scrollToTop}
             animate={controls}
             initial={{opacity:0}}
         >
-            <UpIcon/>
-        </motion.button>
+            <ButtonIcon appearance={'primary'} icon={'up'} onClick={scrollToTop}/>
+        </motion.div>
     );
 };
