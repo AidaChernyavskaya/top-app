48d17364ab126329f900d239e2ef933603f3ab75
Aida <aidachernyavskaya@gmail.com>
2023-01-30 23:28:35 +0300

    scale on buttons added

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -2,11 +2,13 @@ import styles from "./Button.module.css";
 import cn from "classnames";
 import {ButtonProps} from "./Button.props";
 import ArrowIcon from "./arrow.svg";
+import { motion } from "framer-motion";
 
 
 export const Button = ({children, appearance, arrow = 'none', className, ...props}: ButtonProps): JSX.Element => {
     return (
-      <button
+      <motion.button
+          whileHover={{scale: 1.05}}
           className={cn(styles.button, className, {
               [styles.primary]: appearance == 'primary',
               [styles.ghost]: appearance == 'ghost'
@@ -19,6 +21,6 @@ export const Button = ({children, appearance, arrow = 'none', className, ...prop
           })}>
               <ArrowIcon />
           </span>}
-      </button>
+      </motion.button>
     );
 }
